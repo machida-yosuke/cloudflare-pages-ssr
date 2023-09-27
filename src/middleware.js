a3ad68b66853ad1c58269f009928763e7cc1d63f
machida-yosuke <machida-yosuke@kayac.com>
2023-09-27 16:29:52 +0900

    mid

--- src/middleware.js
+++ src/middleware.js
@@ -1,25 +1,34 @@
-const authorization = Astro.request.headers.get("authorization");
+// const authorization = Astro.request.headers.get("authorization");
 
-const BASIC_AUTH = Astro.locals.runtime.env.BASIC_AUTH;
-const BASIC_USERNAME = Astro.locals.runtime.env.BASIC_USERNAME;
-const BASIC_PASSWORD = Astro.locals.runtime.env.BASIC_PASSWORD;
+// const BASIC_AUTH = Astro.locals.runtime.env.BASIC_AUTH;
+// const BASIC_USERNAME = Astro.locals.runtime.env.BASIC_USERNAME;
+// const BASIC_PASSWORD = Astro.locals.runtime.env.BASIC_PASSWORD;
 
-if (authorization) {
-    if (BASIC_AUTH === "true") {
-      const buffer = Buffer.from(pieces[1], "base64");
-      const credentials = buffer.toString();
-      console.log(credentials);
-      const [username, password] = credentials.split(":");
+// if (authorization) {
+//     if (BASIC_AUTH === "true") {
+//       const buffer = Buffer.from(pieces[1], "base64");
+//       const credentials = buffer.toString();
+//       console.log(credentials);
+//       const [username, password] = credentials.split(":");
 
-      if (username === BASIC_USERNAME && password === BASIC_PASSWORD) {
-        Astro.response.status = 200;
-        Astro.response.headers.delete("WWW-Authenticate");
-        Astro.response.headers.delete("Content-Length");
-      }
-    }
+//       if (username === BASIC_USERNAME && password === BASIC_PASSWORD) {
+//         Astro.response.status = 200;
+//         Astro.response.headers.delete("WWW-Authenticate");
+//         Astro.response.headers.delete("Content-Length");
+//       }
+//     }
   
-} else {
-  Astro.response.status = 401;
-  Astro.response.headers.set("WWW-Authenticate", 'Basic realm="realm"');
-  Astro.response.headers.set("Content-Length", "0");
-}
+// } else {
+//   Astro.response.status = 401;
+//   Astro.response.headers.set("WWW-Authenticate", 'Basic realm="realm"');
+//   Astro.response.headers.set("Content-Length", "0");
+// }
+
+export function onRequest ({ locals, request }, next) {
+    // レスポンスデータをリクエストからインターセプトします
+    // 必要に応じて、`locals`を改変してレスポンスを加工します
+    locals.title = "新しいタイトル";
+
+    // Responseか`next()`の結果を返します
+    return next();
+};
